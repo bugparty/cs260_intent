0c6e088f076440c06949292a6d31c8bfbe0d8ff1
bowman <fancycode@gmail.com>
2025-05-30 22:44:27 -0700

    update readme, update 010editor tempate

--- android/os/Parcel.cpp
+++ android/os/Parcel.cpp
@@ -1,3 +1,6 @@
+//see also https://android.googlesource.com/platform/frameworks/native/+/android12-dev/libs/binder/Parcel.cpp
+status_t Parcel::readString16(std::optional<String16>* pArg) const { return readData(pArg); }
+status_t Parcel::readString16(std::unique_ptr<String16>* pArg) const { return readData(pArg); }
 template <typename T>
 binder_status_t ReadArray(const AParcel* parcel, void* arrayData, ArrayAllocator<T> allocator,
                           ArraySetter<T> setter, status_t (Parcel::*read)(T*) const) {
@@ -5,7 +8,9 @@ binder_status_t ReadArray(const AParcel* parcel, void* arrayData, ArrayAllocator
 
     int32_t length;
     status_t status = rawParcel->readInt32(&length);
-
+    //if length is -1,just return
+    // This collapses the statuses into the declared range.
+    //binder_status_t PruneStatusT(android::status_t status);
     if (status != STATUS_OK) return PruneStatusT(status);
     if (length < -1) return STATUS_BAD_VALUE;
 
---
+++ android/os/Parcel.h
@@ -0,0 +1,1 @@
+//https://android.googlesource.com/platform/frameworks/native/+/refs/heads/android12-dev/libs/binder/include/binder/Parcel.h
